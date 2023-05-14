c9b60052d70e8b705185771cab78fbf5c0835f43
vev07 <108584083+vev07@users.noreply.github.com>
2023-05-14 18:40:55 +0200

    cameback Dog.java

--- Lesson2104/src/main/java/Test/Dog.java
+++ Lesson2104/src/main/java/Test/Dog.java
@@ -3,13 +3,7 @@ package Test;
 public class Dog {
     String name;
     public void voice() {
-        System.out.println("
-Cetx
-x
-
-DOG,
-DOG,
-dog is voice");
+     System.out.println("dog is voice");
     }
     protected void eat() {
         System.out.println("It is eating");
