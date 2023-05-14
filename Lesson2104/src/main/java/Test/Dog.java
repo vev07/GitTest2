6567f9e0a68fa2b38ada0d14653ca7899d00e9fe
vev07 <108584083+vev07@users.noreply.github.com>
2023-05-14 18:48:57 +0200

    Dog.java is barcing

--- Lesson2104/src/main/java/Test/Dog.java
+++ Lesson2104/src/main/java/Test/Dog.java
@@ -3,7 +3,7 @@ package Test;
 public class Dog {
     String name;
     public void voice() {
-     System.out.println("dog is voice");
+     System.out.println("dog is barking");
     }
     protected void eat() {
         System.out.println("It is eating");
