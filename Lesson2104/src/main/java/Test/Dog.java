d189d2b320d603a0202367a93865c8796f3950a7
vev07 <108584083+vev07@users.noreply.github.com>
2023-05-14 19:08:57 +0200

    Cat-Dog

--- Lesson2104/src/main/java/Test/Dog.java
+++ Lesson2104/src/main/java/Test/Dog.java
@@ -3,7 +3,7 @@ package Test;
 public class Dog {
     String name;
     public void voice() {
-     System.out.println("dog is voice");
+     System.out.println("Cat-dog is voice");
     }
     protected void eat() {
         System.out.println("It is eating");
