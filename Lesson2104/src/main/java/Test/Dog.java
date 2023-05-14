80f7ae0af792ae7e8fb210ffbafa82dda7fea999
vev07 <108584083+vev07@users.noreply.github.com>
2023-05-14 17:50:32 +0200

    changed dog.java

--- Lesson2104/src/main/java/Test/Dog.java
+++ Lesson2104/src/main/java/Test/Dog.java
@@ -3,7 +3,10 @@ package Test;
 public class Dog {
     String name;
     public void voice() {
-        System.out.println("dog is voice");
+        System.out.println("
+DOG,
+DOG,
+dog is voice");
     }
     protected void eat() {
         System.out.println("It is eating");
