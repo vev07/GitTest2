9ed194d596a23fcd8094f0c9091c13c058f73fed
vev07 <108584083+vev07@users.noreply.github.com>
2023-05-14 18:50:58 +0200

    Revert "Dog.java is barcing"
    This reverts commit 6567f9e0a68fa2b38ada0d14653ca7899d00e9fe.

--- Lesson2104/src/main/java/Test/Dog.java
+++ Lesson2104/src/main/java/Test/Dog.java
@@ -3,7 +3,7 @@ package Test;
 public class Dog {
     String name;
     public void voice() {
-     System.out.println("dog is barking");
+     System.out.println("dog is voice");
     }
     protected void eat() {
         System.out.println("It is eating");
